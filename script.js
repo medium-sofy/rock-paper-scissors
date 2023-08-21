53e8ba7f80f20ce984eb3dcaade3f7c8e6459173
Mohamed Sofy <101935871+medium-sofy@users.noreply.github.com>
2023-08-21 19:41:09 +0300

    Create getRandomInt() function

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+plan: assign a number to each possible choice, 
+eg: rock = 0, paper = 1, and scissors = 2,
+use math.floor(math.random() * 3) to get a random number in the range 0 to 2, 
+check if generated number equals any choice value, if true, return that choice.
+*/
+
+function getRandomInt(int = 3){ //Returns a random integer in the range 0 to < int, defaults to 3.
+	return math.floor(math.random() * int)
+}
