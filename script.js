da1d24ba025de767727ebb423784d8a4cf09a3e9
Mohamed Sofy <101935871+medium-sofy@users.noreply.github.com>
2023-08-25 18:17:44 +0300

    Create getComputerChoice()

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ check if generated number equals any choice value, if true, return that choice.
 function getRandomInt(int = 3){ //Returns a random integer in the range 0 to < int, defaults to 3.
 	return Math.floor(Math.random() * int)
 }
-//function getComputerChoice(){}
+
+function getComputerChoice(){ //Returns either 'Rock', 'Paper', or 'Scissors'
+	let rock = 0 
+	let paper = 1 
+	let scissors = 2
+
+	let choice = getRandomInt()	
+	if(choice === 0){
+		return 'Rock'
+	}
+	else if (choice === 1){
+		return 'Paper'
+	}
+	else {
+		return 'Scissors'
+	}
+}
+
 //function playRound(){}
 //function game(){}
