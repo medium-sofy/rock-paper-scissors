a95ab967f19bcd4823e8bcaba343accc0e0ebca8
Mohamed Sofy <101935871+medium-sofy@users.noreply.github.com>
2023-08-25 19:32:30 +0300

    Create playRound()

--- script.js
+++ script.js
@@ -16,15 +16,66 @@ function getComputerChoice(){ //Returns either 'Rock', 'Paper', or 'Scissors'
 
 	let choice = getRandomInt()	
 	if(choice === 0){
-		return 'Rock'
+		return 'rock'
 	}
 	else if (choice === 1){
-		return 'Paper'
+		return 'paper'
 	}
 	else {
-		return 'Scissors'
+		return 'scissors'
 	}
 }
 
-//function playRound(){}
+/*
+	function playRound() pseudocode: 
+
+if (player === computer){
+	It's A draw!
+}
+else if (player = rock && computer = paper){
+	return 'you lose! paper beats ${playerSelection}'
+}
+else if(player = rock && computer = scissors){
+	return 'You win! ${playerSelection} beats Scissors!'
+}
+else if(player = paper && computer = rock){
+	return 'You win!  ${playerSelection} beats rock'
+}
+else if(player = paper && computer = scissors){
+	return 'You lose! scissors beats ${playerSelection}'
+}
+else if (player = scissors && computer = rock){
+	return 'You lose! rock beats ${playerSelection}'
+}
+else if (player = scissors && computer = paper){
+	return 'you win! ${playerSelection beats Paper!}'
+}
+*/
+function playRound(playerSelection, computerSelection){
+	
+	playerInsensitive = playerSelection.toLowerCase() // converts playerSelection to lower case, to make it case-insensitive.
+	
+	if(playerInsensitive === computerSelection){
+		return "It's a Draw!"
+	}
+	else if (playerInsensitive === 'rock' && computerSelection === 'paper'){
+		return `You lose! Paper beats ${playerSelection}`
+	}
+	else if(playerInsensitive === 'rock' && computerSelection === 'scissors'){
+		return `You win! ${playerSelection} beats scissors!`
+	}
+	else if(playerInsensitive === 'paper' && computerSelection=== 'rock'){
+		return `You win! ${playerSelection} beats rock`
+	}
+	else if(playerInsensitive === 'paper' && computerSelection === 'scissors'){
+		return `You lose! Scissors beats ${playerSelection}`
+	}
+	else if (playerInsensitive === 'scissors' && computerSelection === 'rock'){
+		return `You lose! Rock beats ${playerSelection}`
+	}
+	else if (playerInsensitive === 'scissors' && computerSelection === 'paper'){
+		return `You win! ${playerSelection} beats paper!`
+	}
+}
+
 //function game(){}
